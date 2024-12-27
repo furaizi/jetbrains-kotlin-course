480b356184ce43100b9875ca1c7662d5146a1bd0
furaizi <furachannel16@gmail.com>
2024-12-27 21:37:56 +0200

    Standard library examples lesson

--- src/main/kotlin/module_2/extensions/examples_SL/StandartLibraryExamples.kt
+++ src/main/kotlin/module_2/extensions/examples_SL/StandartLibraryExamples.kt
@@ -1,2 +1,33 @@
 package module_2.extensions.examples_SL
 
+/*
+Kotlin standard library = Java standard library + extensions
+
+Functions:
+Iterable<T>.joinToString
+Array<T>.getOrNull
+Iterable<T>.withIndex()
+infix Int.until -    1.until(10)     1 until 10
+infix <A, B> A.to(that: B)
+
+
+Extensions on String:
+
+have text block like in Java
+val q = """To code,
+    |or not to code?..""".trimMargin()
+to get normal alignment
+
+regex
+val regex = "\\w+".toRegex()
+regex.matches("str") -> true
+
+toInt()
+toDouble()
+toIntOrNull()
+ */
+
+fun main() {
+    val set = hashSetOf(1, 7)
+    println(set.javaClass) // class java.util.HashSet
+}
