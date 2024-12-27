82d0bfa044ab65d379a6645d18b94a6814bd1747
furaizi <furachannel16@gmail.com>
2024-12-27 20:27:43 +0200

    Loops lesson

--- src/main/kotlin/module_2/control_structures/loops/Loops.kt
+++ src/main/kotlin/module_2/control_structures/loops/Loops.kt
@@ -1,2 +1,49 @@
 package module_2.control_structures.loops
 
+/*
+while (condition) {
+    ...
+}
+
+the same do-while
+
+
+for loop:
+for (str in list) {
+    print(s)
+}
+or
+for (str: String in list) {
+    print(s)
+}
+
+
+
+Iterating over map
+val map = mapOf(1 to "one", 2 to "two", 3 to "three)
+for ((key, value) in map) {
+    println("$key = $value")
+}
+
+
+
+Iterating with index
+for ((index, element) in list.withIndex()) {
+    ...
+}
+
+
+Iterating over range
+for (i in 1..9) // 123456789
+for (c in '0'..'9')
+for (i in 1 until 9) // 12345678
+
+
+Iterating with a step
+for (i in 9 downTo 1 step 2)
+
+
+
+Iterating over String
+for (ch in "abc")
+ */
