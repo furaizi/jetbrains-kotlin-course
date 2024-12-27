af50c056a595ce5cfaa6e9a26faceb5163b12773
furaizi <furachannel16@gmail.com>
2024-12-27 21:46:48 +0200

    Calling extensions lesson

--- src/main/kotlin/module_2/extensions/calling_extension/CallingExtension.kt
+++ src/main/kotlin/module_2/extensions/calling_extension/CallingExtension.kt
@@ -1,2 +1,6 @@
 package module_2.extensions.calling_extension
 
+/*
+As extensions are static Java functions under the hood, there is no override for extension functions in Kotlin
+but extensions can overload members
+ */
