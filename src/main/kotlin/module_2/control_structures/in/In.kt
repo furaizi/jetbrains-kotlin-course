948e23bf6dd6cb92b3f92ba86fc6bcf8831eeda1
furaizi <furachannel16@gmail.com>
2024-12-27 20:54:46 +0200

    Playground lesson

--- src/main/kotlin/module_2/control_structures/in/In.kt
+++ src/main/kotlin/module_2/control_structures/in/In.kt
@@ -1,2 +1,32 @@
 package module_2.control_structures.`in`
 
+/*
+
+in: two use cases
+1. iteration
+2. check for belonging
+c in 'a'..'z'
+c !in '0'..'9'
+
+
+You can use "in" in "when"
+when (c) {
+    in 'a'..'z', in 'A'..'Z' -> do()
+}
+
+1..9, 1 until 10 IntRange
+'a'..'z' CharRange
+"ab".."az" ClosedRange<String>
+startDate..endDate ClosedRange<MyDate>
+
+
+"Kotlin" in "Java".."Scala" // true
+
+
+To use a range of custom types, the class you are creating must implement Comparable<$Type>
+class MyDate : Comparable<MyDate>
+
+
+Belonging to collection
+if (element in list)
+ */
--- src/main/kotlin/module_2/control_structures/playground/Playground.kt
+++ src/main/kotlin/module_2/control_structures/playground/Playground.kt
@@ -1,2 +1,16 @@
 package module_2.control_structures.playground
 
+import java.util.regex.Pattern
+
+fun isValidIdentifier(s: String): Boolean {
+    return s.isNotEmpty() && Pattern.matches("[a-zA-Z_]", s[0].toString()) && Pattern.matches("\\w+", s)
+}
+
+fun main(args: Array<String>) {
+    println(isValidIdentifier("name"))   // true
+    println(isValidIdentifier("_name"))  // true
+    println(isValidIdentifier("_12"))    // true
+    println(isValidIdentifier(""))       // false
+    println(isValidIdentifier("012"))    // false
+    println(isValidIdentifier("no$"))    // false
+}
