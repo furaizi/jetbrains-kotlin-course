a113b47c6c8e5a6c8174ac42e26edbcd23370ccd
furaizi <furachannel16@gmail.com>
2024-12-27 18:34:09 +0200

    Variables lesson

---
+++ src/main/kotlin/module_2/basics/Variables.kt
@@ -0,0 +1,23 @@
+package module_2.basics
+
+/*
+Two types of variables:
+1. val (value) - read-only, assigned-once
+corresponds to a final var in java
+2. var (variable) - mutable
+
+Kotlin is static-typed language, every variable has its own type
+but there is no need to explicitly specify a type of the variable if it's obvious from context
+
+General advice: prefer vals to vars
+ */
+
+/*
+Two types of lists:
+1. mutable
+val mutableList = mutableListOf("Java")
+
+2. read-only
+val readOnlyList = listOf("Java")
+read-only list lacks mutating methods
+ */
