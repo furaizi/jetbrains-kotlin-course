4311bfc12ccb533d292f85a748f3e41ade6709fb
furaizi <furachannel16@gmail.com>
2024-12-27 21:38:19 +0200

    Playground lesson

--- src/main/kotlin/module_2/extensions/playground/Playground.kt
+++ src/main/kotlin/module_2/extensions/playground/Playground.kt
@@ -1,2 +1,14 @@
 package module_2.extensions.playground
 
+fun List<Int>.sum(list: List<Int>): Int {
+    var result = 0
+    for (i in list) {
+        result += i
+    }
+    return result
+}
+
+fun main(args: Array<String>) {
+    val sum = listOf(1, 2, 3).sum()
+    println(sum)    // 6
+}
