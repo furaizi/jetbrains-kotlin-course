96a1bef66cf1791db334644e0da52d837106923c
furaizi <furachannel16@gmail.com>
2024-12-27 21:03:41 +0200

    Exceptions lesson

--- src/main/kotlin/module_2/control_structures/exceptions/Exceptions.kt
+++ src/main/kotlin/module_2/control_structures/exceptions/Exceptions.kt
@@ -1,2 +1,13 @@
 package module_2.control_structures.exceptions
 
+/*
+No difference between checked and unchecked exceptions
+
+throw is an expression
+val percentage = throw Exception()
+
+try is an expression
+
+There is @Throws annotation
+It is only need for better usability from Java code
+ */
