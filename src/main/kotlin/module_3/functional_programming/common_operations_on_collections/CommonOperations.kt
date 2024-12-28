504209ebda24bdf997501b4dfeef1ec87bbb09e6
furaizi <furachannel16@gmail.com>
2024-12-28 19:15:37 +0200

    Common operations on collections lesson

---
+++ src/main/kotlin/module_3/functional_programming/common_operations_on_collections/CommonOperations.kt
@@ -0,0 +1,19 @@
+package module_3.functional_programming.common_operations_on_collections
+
+/*
+
+Operations:
+    filter
+    map
+    any, all, none
+    find, first, firstOrNull
+    count
+    partition - divides collection into two collection by given predicate
+    groupBy - groups elements by given key (creates a map with key and a group)
+    associateBy - maps key to unique element (duplicates removed, last element will be chosen)
+    associate - maps one value to another value { 'a' + it to 10 * it }
+    zip - makes one list of tuples of two lists by each element
+    zipWithNext - {1, 2, 3, 4} -> {(1, 2), (2, 3), (3, 4)}
+    flatten
+    flatMap
+ */
