da87540fa9135bc77700928d7df9f969ae3b5a5d
furaizi <furachannel16@gmail.com>
2024-12-28 18:41:15 +0200

    Importance of nullable types lesson

---
+++ src/main/kotlin/module_3/nullability/importance_of_nullability/Importance.kt
@@ -0,0 +1,9 @@
+package module_3.nullability.importance_of_nullability
+
+/*
+null is "a billion-dollar mistake"
+NPE is hard to debug
+Although Optional class exists, nullable types are not wrappers on objects and dont use extra memory
+And it's much more convenient to use nullable types than Optionals
+It's considered one of the most useful features in Kotlin
+ */
