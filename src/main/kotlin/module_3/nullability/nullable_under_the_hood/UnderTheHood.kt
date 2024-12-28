c44ad09228f1b943dfef046d9d230a28d60829a9
furaizi <furachannel16@gmail.com>
2024-12-28 18:19:51 +0200

    Nullable types under the hood lesson

---
+++ src/main/kotlin/module_3/nullability/nullable_under_the_hood/UnderTheHood.kt
@@ -0,0 +1,8 @@
+package module_3.nullability.nullable_under_the_hood
+
+/*
+Nullable types are implemented using @Nullable, @NotNull annotations
+
+Optional != nullable types
+Optional is a wrapper than can cause performance, since nullable types dont create any wrappers
+ */
