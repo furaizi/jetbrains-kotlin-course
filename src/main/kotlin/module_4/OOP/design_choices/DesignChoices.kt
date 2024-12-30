cedf7b1c9b4a157fcc5bfad3fffcfd03caab2c31
furaizi <furachannel16@gmail.com>
2024-12-30 17:07:53 +0200

    OOP design choices lesson

---
+++ src/main/kotlin/module_4/OOP/design_choices/DesignChoices.kt
@@ -0,0 +1,7 @@
+package module_4.OOP.design_choices
+
+/*
+"public" and "final" by default is a compromise between application and library developers
+"public" fits the best to application developers
+"final" - to library ones
+ */
