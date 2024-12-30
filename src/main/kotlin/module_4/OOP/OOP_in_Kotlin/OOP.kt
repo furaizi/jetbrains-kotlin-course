c6227fb52a396801ef99b8260244632bd164da0a
furaizi <furachannel16@gmail.com>
2024-12-30 12:54:24 +0200

    OOP in Kotlin lesson

---
+++ src/main/kotlin/module_4/OOP/OOP_in_Kotlin/OOP.kt
@@ -0,0 +1,19 @@
+package module_4.OOP.OOP_in_Kotlin
+
+/*
+Every class is "public" and "final" by default
+Mark it as "open" if you want to allow inheritance
+
+"internal" - visible in the same module
+"protected" - visible in the class and its subclasses
+"private" class member - visible in the class
+"private" top-level declaration - visible in the file
+Under the hood, "internal" is public, but obfuscates a method name
+
+Module is a set of Kotlin files compiled together
+- an IDEA module
+- a Maven project
+- a Gradle source set
+
+In Kotlin you can put several classes into one file
+ */
