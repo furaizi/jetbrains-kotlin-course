598a349a9dc41280ce4a45a600c98a9460aa0b09
furaizi <furachannel16@gmail.com>
2024-12-30 18:20:10 +0200

    Not using operation overloading lesson

---
+++ src/main/kotlin/module_4/conventions/not_using_operator_overloading/NotUsing.kt
@@ -0,0 +1,6 @@
+package module_4.conventions.not_using_operator_overloading
+
+/*
+In Kotlin you can't make your own operations
+You shouldn't abuse operator overloading, use it only it's natural to use this kind of operator
+ */
