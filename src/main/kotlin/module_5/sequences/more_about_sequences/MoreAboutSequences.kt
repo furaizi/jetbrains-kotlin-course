56a706d29fccbdedaf1a94e9b4ef8e1bc5a1c9a5
furaizi <furachannel16@gmail.com>
2024-12-31 18:54:53 +0200

    More about sequences lesson

---
+++ src/main/kotlin/module_5/sequences/more_about_sequences/MoreAboutSequences.kt
@@ -0,0 +1,9 @@
+package module_5.sequences.more_about_sequences
+
+/*
+Sequences, like Java Streams, implements Lazy approach
+This means that computations will be conducted only when the result is needed (terminal operation is called)
+
+Collections - horizontal evaluation (collection by collection)
+Sequences - vertical evaluation (operator by operator)
+ */
