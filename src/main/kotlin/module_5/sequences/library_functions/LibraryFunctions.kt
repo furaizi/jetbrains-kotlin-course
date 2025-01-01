fdbff7b309fb370b5bb17b9a99c83d6be66d7cc9
furaizi <furachannel16@gmail.com>
2025-01-01 23:06:11 +0200

    Library functions lesson

---
+++ src/main/kotlin/module_5/sequences/library_functions/LibraryFunctions.kt
@@ -0,0 +1,13 @@
+package module_5.sequences.library_functions
+
+/*
+filter.size -> count
+sortedBy.reversed -> sortedByDescending
+map.filterNotNull -> mapNotNull
+
+if (person.age !in map) {
+  map[person.age] = mutableListOf()
+}
+map.getValue(person.age) += person
+to getOrPut or better groupBy
+ */
