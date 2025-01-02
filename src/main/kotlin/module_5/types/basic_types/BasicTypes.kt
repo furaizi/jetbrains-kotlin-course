e865e27cc7997382e20fbfec8c5a74f06948a05d
furaizi <furachannel16@gmail.com>
2025-01-02 07:40:28 +0200

    Basic types lesson

---
+++ src/main/kotlin/module_5/types/basic_types/BasicTypes.kt
@@ -0,0 +1,17 @@
+package module_5.types.basic_types
+
+/*
+There are no primitives in the language
+
+Correspondence between Kotlin and Java types
+Int = int
+Int? = Integer
+List<Int> = List<Integer>
+Array<Int> = Integer[]
+IntArray = int[]
+Any = Object
+() -> Boolean = Function0<Boolean>
+(Order) -> Int = Function1<Order, Int>
+
+Prefer Lists to Arrays
+ */
