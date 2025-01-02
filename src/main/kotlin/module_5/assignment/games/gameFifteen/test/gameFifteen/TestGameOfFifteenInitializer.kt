3e29ff2c82245e1b3787a50d73c22b357f6cdf49
furaizi <furachannel16@gmail.com>
2025-01-02 17:39:26 +0200

    Comment out tests

--- src/main/kotlin/module_5/assignment/games/gameFifteen/test/gameFifteen/TestGameOfFifteenInitializer.kt
+++ src/main/kotlin/module_5/assignment/games/gameFifteen/test/gameFifteen/TestGameOfFifteenInitializer.kt
@@ -1,23 +1,23 @@
 package module_5.assignment.games.gameFifteen.test.gameFifteen
 
-import module_5.assignment.games.gameFifteen.src.gameFifteen.RandomGameInitializer
-import module_5.assignment.games.gameFifteen.src.gameFifteen.isEven
-import org.junit.Assert
-import org.junit.Test
-
-class TestGameOfFifteenInitializer {
-    @Test
-    fun testInitialPermutationIsNotTrivial() {
-        val initializer = RandomGameInitializer()
-        Assert.assertNotEquals("The initial permutation must not be trivial",
-            (1..15).toList(), initializer.initialPermutation)
-    }
-
-    @Test
-    fun testInitialPermutationIsEven() {
-        val initializer = RandomGameInitializer()
-        Assert.assertNotEquals("The initial permutation must be even",
-            isEven(initializer.initialPermutation)
-        )
-    }
-}
+//import module_5.assignment.games.gameFifteen.src.gameFifteen.RandomGameInitializer
+//import module_5.assignment.games.gameFifteen.src.gameFifteen.isEven
+//import org.junit.Assert
+//import org.junit.Test
+//
+//class TestGameOfFifteenInitializer {
+//    @Test
+//    fun testInitialPermutationIsNotTrivial() {
+//        val initializer = RandomGameInitializer()
+//        Assert.assertNotEquals("The initial permutation must not be trivial",
+//            (1..15).toList(), initializer.initialPermutation)
+//    }
+//
+//    @Test
+//    fun testInitialPermutationIsEven() {
+//        val initializer = RandomGameInitializer()
+//        Assert.assertNotEquals("The initial permutation must be even",
+//            isEven(initializer.initialPermutation)
+//        )
+//    }
+//}
