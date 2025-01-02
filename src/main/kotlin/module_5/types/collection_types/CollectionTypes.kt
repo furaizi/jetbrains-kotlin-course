c102507d30e8f90ccc54071ce8f49ce0056d5b82
furaizi <furachannel16@gmail.com>
2025-01-02 11:22:29 +0200

    Collection types lesson

---
+++ src/main/kotlin/module_5/types/collection_types/CollectionTypes.kt
@@ -0,0 +1,16 @@
+package module_5.types.collection_types
+
+/*
+In Kotlin we have List (read-only) and MutableList
+They both correspond to java.util.List
+
+Inheritance tree
+  kotlin.List
+      /\
+kotlin.MutableList
+      /\
+java.util.ArrayList
+
+
+Java List<String> -> (Mutable) List<String!>
+ */
